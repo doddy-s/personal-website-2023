20f9714eab8e653e8c89ac213c7c148c40e9ad17
Doddy Sujatmiko <mdoddys1604@gmail.com>
2023-01-29 21:04:47 +0700

    adding animation and breakpoint to navbar

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,22 +1,30 @@
 export default function NavBar() {
     return (
-      <div className="fixed top-0 w-screen h-12 p-4 m-0 flex flex-row justify-start items-center gap-4 bg-black bg-opacity-50 text-white shadow">
+      <div className="fixed top-0 w-screen h-12 px-4 py-4 m-0 flex flex-row justify-start items-center gap-4 bg-black bg-opacity-50 text-white shadow">
         <Icon />
-        <Button content="Home" />
-        <Button content="Hobby" />
-        <Button content="Contact" />
+        <div className="flex flex-row justify-start items-center gap-4 collapse md:visible">
+            <Button content="Home" />
+            <Button content="Hobby" />
+            <Button content="Contact" />
+        </div>
+        
       </div>
     );
 };
 
 const Button = ({ content }: any) => (
-    <div className="w-24 h-8 rounded-lg bg-black bg-opacity-50 text-center">
-        {content}
-    </div>
+    <a href="/">
+        <div className="flex flex-row items-center justify-center w-24 h-10 rounded-lg 
+        transition ease-in-out duration-1000 
+        hover:bg-black hover:bg-opacity-50 hover:shadow-lg">
+            {content}
+        </div>
+    </a>
+    
 )
 
 const Icon = () => (
-    <div className="w-8 h-8 rounded-full">
+    <div className="w-10 h-10 rounded-full">
         <img className="rounded-full" src="https://github.com/doddy-s.png" alt="Should be an icon here" />
     </div>
 )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import NavBar from "./_components/NavBar";
+import ArrowDown from "./_components/ArrowDown";
 
 export default function Home() {
   return (
@@ -7,14 +8,15 @@ export default function Home() {
       <div className="h-screen bg-zinc-900">
         <div className="h-screen flex flex-col justify-between items-center">
           <div>
-            <div className="h-12"></div>
-            <h1 className="text-center text-white font-bold">Mohamad Doddy Sujatmiko</h1>
+            <div className="h-32"></div>
+            <h1 className="text-center text-4xl text-white font-bold">Mohamad Doddy Sujatmiko</h1>
           </div>
           <div>
               <img className="rounded-full" src="https://github.com/doddy-s.png" alt="Should be an icon here" />
           </div>
-          <div>
+          <div className="flex flex-col items-center">
             <h1 className="text-center text-white font-bold">Scroll Down</h1>
+            <ArrowDown />
           </div>
         </div>
         <div className="h-screen"></div>
