c4b750030ee066c38c9f9cc7e7201d9abc7f30c1
Doddy Sujatmiko <mdoddys1604@gmail.com>
2023-01-31 16:29:26 +0700

    converts img to Image

--- src/app/skills/loading.tsx
+++ src/app/skills/loading.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image"
+
 export default function loading() {
     return (
         <div className="w-auto h-screen flex flex-col justify-center items-center gap-4">
-            <img className="animate-spin w-24 h-24" src="https://cdn.discordapp.com/attachments/1069729310262435860/1069891046839963729/loading.svg" alt="loading" />
+            <Image className="animate-spin w-20 h-20" src="https://cdn.discordapp.com/attachments/1069729310262435860/1069891046839963729/loading.svg" alt="loading" 
+            width={80} height={80} />
             <h1 className="text-white">Loading....</h1>
         </div>
     )
